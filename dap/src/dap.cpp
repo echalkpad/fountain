347e1bfd2c8af96a8db2d6364c398f1c1e057e72
Doug Johnson <finson@whidbey.com>
2014-03-16 21:37:30 -0700

    Actually try to read the device registers.
    Doesn't work, but what the heck, it's a start.

--- dap/src/dap.cpp
+++ dap/src/dap.cpp
@@ -17,36 +17,30 @@
 #include <sys/ioctl.h>
 #include <stropts.h>
 #include <math.h>
-
-
-typedef struct device {
-	char *name;
-	int bus;
-	int address;
-	int handle;
-} device;
-
-device accel;
-
-char *busname[] = {"/dev/i2c-0","/dev/i2c-1","/dev/i2c-2"};
+#include "LSM303Accelerometer.h"
 
 int main(int argc, char* argv[]) {
 	printf("DAP - I2C Data Acquisition Program\n");
 
-	// print some info about the targeted device
+	LSM303Accelerometer *d = new LSM303Accelerometer(1,0x19,"LSM303 Accelerometer");
 
-	accel.name = "LSM303 Accelerometer";
-	accel.bus = 1;
-	accel.address = 0x19;
+	printf("Device: %s at Bus %i, Address %#x.\n",d->getDeviceName(),d->getBus(),d->getDeviceAddress());
 
-	printf("Device: %s at Bus %i, Address %#x.\n",accel.name,accel.bus,accel.address);
-
-	int status = openDevice(accel);
-	if (status != 0) {
-		printf("Open error: %i\n", status);
+	int status = d->openDevice();
+	if (status < 0) {
 		return EXIT_FAILURE;
 	}
-	printf("Device opened.  Handle: %i\n",accel.handle);
+	printf("Device opened.  Handle: %i\n",d->getHandle());
+
+	status = d->readFullSensorState();
+	if (status < 0) {
+		return EXIT_FAILURE;
+	}
+
+	status = d->closeDevice();
+	if (status < 0) {
+		return EXIT_FAILURE;
+	}
 
 	// set control registers
 
@@ -55,21 +49,3 @@ int main(int argc, char* argv[]) {
 	return EXIT_SUCCESS;
 }
 
-int openDevice(device d) {
-	int status = open(busname[d.bus], O_RDWR);
-
-	if (status < 0) {
-			printf("Failed to open %s on I2C Bus %s.  Error: %i\n", d.name,busname[d.bus],status);
-			return(status);
-	}
-
-	d.handle = status;
-	status = ioctl(d.handle, I2C_SLAVE, d.address);
-
-	if (status < 0){
-			printf("I2C_SLAVE address %#x failed.  Error: %i\n",d.address,status);
-			return(status);
-	}
-
-	return 0;
-}
