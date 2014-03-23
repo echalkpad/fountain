a28ac633ea064f7f8371236051d106c39c483d6f
Doug Johnson <finson@whidbey.com>
2014-03-22 21:32:27 -0700

    Seg fault was due to printf oversight.

--- dap/src/dapLSM303.cpp
+++ dap/src/dapLSM303.cpp
@@ -30,24 +30,24 @@ int main(int argc, char* argv[]) {
 	printf("Device initialization complete: %s at Bus %i, Address %#04x. Handle: %i\n", d1->getDeviceName(),
 			d1->getBus(), d1->getDeviceAddress(),d1->getHandle());
 
-//	LSM303D *d2 = new LSM303D(1,0x1D,"LSM303D");
-//	printf("Device initialization complete: %s at Bus %i, Address %#04x. Handle: %i\n", d2->getDeviceName(),
-//		d2->getBus(), d2->getDeviceAddress(),d2->getHandle());
+	LSM303D *d2 = new LSM303D(1,0x1D,"LSM303D");
+	printf("Device initialization complete: %s at Bus %i, Address %#04x. Handle: %i\n", d2->getDeviceName(),
+		d2->getBus(), d2->getDeviceAddress(),d2->getHandle());
 
 	for (int idx = 0; idx < 50; idx++) {
 
 		d1->refreshSensorData();
-//		d2->refreshSensorData();
+		d2->refreshSensorData();
 
 		printf("%-10s Acc: X, Y, Z: %f, %f, %f.  Temp: %f\n",
 				d1->getDeviceName(),d1->getAccelerationX(),d1->getAccelerationY(),d1->getAccelerationZ(),d1->getTemperature());
-		fprintf(datafile,"%i,%s,%f,%f,%f\n",
+		fprintf(datafile,"%s,%i,%f,%f,%f\n",
 				d1->getDeviceName(),idx,d1->getAccelerationX(),d1->getAccelerationY(),d1->getAccelerationZ());
 
-//		printf("%-10s Acc: X, Y, Z: %f, %f, %f.  Temp: %f\n",
-//				d2->getDeviceName(),d2->getAccelerationX(),d2->getAccelerationY(),d2->getAccelerationZ(),d2->getTemperature());
-//		fprintf(datafile,"%i,%s,%f,%f,%f\n",
-//				d2->getDeviceName(),idx,d2->getAccelerationX(),d2->getAccelerationY(),d2->getAccelerationZ());
+		printf("%-10s Acc: X, Y, Z: %f, %f, %f.  Temp: %f\n",
+				d2->getDeviceName(),d2->getAccelerationX(),d2->getAccelerationY(),d2->getAccelerationZ(),d2->getTemperature());
+		fprintf(datafile,"%s,%i,%f,%f,%f\n",
+				d2->getDeviceName(),idx,d2->getAccelerationX(),d2->getAccelerationY(),d2->getAccelerationZ());
 
 		sleep(1);
 	}
