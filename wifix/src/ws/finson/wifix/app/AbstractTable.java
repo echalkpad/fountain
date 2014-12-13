068c40d5cfe617225469ef900cea56847ebde1b1
Doug Johnson <finson@whidbey.com>
2014-12-13 06:14:24 -0800

    First small steps towards generic tables.

---
+++ wifix/src/ws/finson/wifix/app/AbstractTable.java
@@ -0,0 +1,23 @@
+package ws.finson.wifix.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ *
+ * @author Doug Johnson
+ * @since Dec 13, 2014
+ *
+ */
+public abstract class AbstractTable implements DAP_Table {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 
+     */
+    public AbstractTable() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ wifix/src/ws/finson/wifix/app/DAP_Table.java
@@ -0,0 +1,13 @@
+package ws.finson.wifix.app;
+
+/**
+ * Methods that a class implements in order to model a data table.
+ *
+ * @author Doug Johnson
+ * @since Dec 13, 2014
+ *
+ */
+public interface DAP_Table {
+
+    
+}
