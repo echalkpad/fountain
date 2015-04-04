81a256eecb9ef9077cf0e6934aad77761ed7f2c9
Doug Johnson <finson@whidbey.com>
2015-04-03 22:51:36 -0700

    Use cylon to generate draft gatt module files.

---
+++ cylon/src/cylon-gatt-ess-client/spec/lib/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var GattEssClient = source("adaptor");
+
+describe("Cylon.Adaptors.GattEssClient", function() {
+  var adaptor = new GattEssClient();
+
+  it("needs tests");
+});
---
+++ cylon/src/cylon-gatt-ess-client/spec/lib/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var GattEssClient = source("driver");
+
+describe("Cylon.Drivers.GattEssClient", function() {
+  var driver = new GattEssClient({
+    connection: {}
+  });
+
+  it("needs tests");
+});
