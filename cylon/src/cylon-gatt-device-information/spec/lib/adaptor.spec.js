b864c04941354270e0162ff75a22c9463e95d462
Doug Johnson <finson@whidbey.com>
2015-04-06 08:36:06 -0700

    Start to create a peripheral device module.

---
+++ cylon/src/cylon-gatt-device-information/spec/lib/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var GattDeviceInformation = source("adaptor");
+
+describe("Cylon.Adaptors.GattDeviceInformation", function() {
+  var adaptor = new GattDeviceInformation();
+
+  it("needs tests");
+});
---
+++ cylon/src/cylon-gatt-device-information/spec/lib/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var GattDeviceInformation = source("driver");
+
+describe("Cylon.Drivers.GattDeviceInformation", function() {
+  var driver = new GattDeviceInformation({
+    connection: {}
+  });
+
+  it("needs tests");
+});
