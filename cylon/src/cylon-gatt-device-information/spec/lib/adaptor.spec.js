f46ddd57484bea6791ece393acda16e0059bce87
Doug Johnson <finson@whidbey.com>
2015-04-16 08:09:13 -0700

    Consolidate development folders.

--- cylon/src/cylon-gatt-device-information/spec/lib/adaptor.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var GattDeviceInformation = source("adaptor");
-
-describe("Cylon.Adaptors.GattDeviceInformation", function() {
-  var adaptor = new GattDeviceInformation();
-
-  it("needs tests");
-});
--- cylon/src/cylon-gatt-ess-client/spec/lib/adaptor.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var GattEssClient = source("adaptor");
-
-describe("Cylon.Adaptors.GattEssClient", function() {
-  var adaptor = new GattEssClient();
-
-  it("needs tests");
-});
