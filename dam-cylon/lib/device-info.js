23974aacd14500cb9991d172725e0fbf4835a2d3
Doug Johnson <finson@whidbey.com>
2015-04-27 09:47:24 -0700

    Start cylon device info robot object.

---
+++ dam-cylon/lib/device-info.js
@@ -0,0 +1,31 @@
+/**
+ * This module looks for a device information service available over BLE and
+ * then reads data from it.
+ */
+
+"use strict";
+
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connections: {
+    bluetooth: { adaptor: 'ble', uuid: '7dc34621191a4e8d87e9747f561038f6' }
+  },
+
+  devices: {
+    deviceInfo: { driver: 'ble-device-information' }
+  },
+
+  display: function(err, data) {
+    if (!!err) {
+      console.log("Error: ", err);
+      return;
+    }
+
+    console.log("callback for getManufacturerName")
+    console.log("Data: ", data);
+  },
+
+  work: function(my) {
+    my.deviceInfo.getManufacturerName(my.display);
+  }}).start();
