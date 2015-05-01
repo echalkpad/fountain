94d9318399d82c86a6cd591079e576baa64e768d
Doug Johnson <finson@whidbey.com>
2015-04-30 22:59:43 -0700

    Work on device info app based on cylon.

--- dam-cylon/device-info.js
+++ dam-cylon/device-info.js
@@ -3,49 +3,47 @@
  * then reads data from it.
  */
 
-"use strict";
+ "use strict";
 
-var Cylon = require('cylon');
-var noble = require('noble');
+ var Cylon = require('cylon');
+ var noble = require('noble');
+ var services = require('./services.json');
 
-Cylon.robot({
+noble.on('discover', function(peripheral) {
+  console.log("Peripheral discovered!")
+  console.log("  Name: " + peripheral.advertisement.localName)
+  console.log("  UUID: " + peripheral.uuid);
+  console.log("  rssi: " + peripheral.rssi);
+});
+
+ Cylon.robot({
   connections: {
-    bluetooth: { adaptor: 'ble', uuid: 'eb2299410ba7' }
+    bluetooth: { adaptor: 'ble', uuid: 'ca226ae6eb4b' }
   },
 
   devices: {
     deviceInfo: { driver: 'ble-device-information' }
   },
 
-  display: function(err, data) {
-    if (!!err) {
-      console.log("Error: ", err);
-      return;
-    }
-
-    console.log("callback for getManufacturerName")
-    console.log("Data: ", data);
-  },
   work: function(my) {
+    console.log("############################ Connection 'bluetooth'");
     console.log(my.bluetooth);
-    console.log(my.bluetooth.peripherals);
-    my.deviceInfo.getManufacturerName(my.display);
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Connected Peripherals');
+    console.log(my.bluetooth.connectedPeripherals);
+    for (var i in my.bluetooth.connectedPeripherals) {
+      console.log('++++++++++++++ '); 
+      console.log("Peripheral Key (uuid): "+i);
+      console.log("Advertised Local Name: "+my.bluetooth.connectedPeripherals[i].peripheral.advertisement.localName);
+      console.log('--');
+      console.log(my.bluetooth.connectedPeripherals[i]);
+      console.log('-- Services');
+      for (var idx in my.bluetooth.connectedPeripherals[i].peripheral.advertisement.serviceUuids) {
+        var su = my.bluetooth.connectedPeripherals[i].peripheral.advertisement.serviceUuids[idx];
+        console.log("idx: "+idx+", service uuid: "+su);
+  //     console.log(my.bluetooth.connectedPeripherals[i].peripheral.advertisement.serviceUuids[su]);
+  //       console.log(su+": "+services[su].name+", "+services[su].type);
+      }
+      console.log('--------------');
+    };   
 
   }}).start();
-
-// noble.on('stateChange', function(state) {
-//   if (state === 'poweredOn') {
-//     console.log("Starting scan.");
-//     noble.startScanning();
-//   } else {
-//     console.log("Stopping scan.");
-//     noble.stopScanning();
-//   }
-// });
-
-// noble.on('discover', function(peripheral) {
-//   console.log("Peripheral discovered!")
-//   console.log("  Name: " + peripheral.advertisement.localName)
-//   console.log("  UUID: " + peripheral.uuid);
-//   console.log("  rssi: " + peripheral.rssi);
-// });
