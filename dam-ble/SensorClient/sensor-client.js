f4b0b437a9c266fbe324931e4564c387a5e5defe
Doug Johnson <finson@whidbey.com>
2015-05-01 21:28:30 -0700

    Tidy up the discovery process a little bit.

--- dam-ble/SensorClient/sensor-client.js
+++ dam-ble/SensorClient/sensor-client.js
@@ -1,29 +1,35 @@
 #!/usr/bin/env node
 
 var noble = require('noble');
+var services = require('./services.json');
 
 var scanStartTime;
 var date = new Date();
+var servers = {};
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
-    console.log("Starting scan.");
+    console.log("\nStarting scan.");
     scanStartTime = date.getTime();
     noble.startScanning();
   } else {
     noble.stopScanning();
     var scanStopTime = date.getTime();
-    console.log("Stopping scan after "+(scanStopTime-scanStartTime)+ " ms.");
+    console.log("\nStopping scan after "+(scanStopTime-scanStartTime)+ " ms.");
   }
 });
 
 noble.on('discover', function(peripheral) {
-  console.log("+++++++ Peripheral discovered.")
+  console.log("\n+++++++ Peripheral discovered.")
   console.log("   Name: " + peripheral.advertisement.localName)
   console.log("   UUID: " + peripheral.uuid);
   console.log("address: " + peripheral.address);
-  console.log(" advert: " + peripheral.advertisement);
-  console.log("-------");
-  console.log(peripheral);
-  console.log("=======");
+
+  servers[peripheral.uuid] = peripheral;
+  var arrayLength = peripheral.advertisement.serviceUuids.length;
+  for (var i = 0; i < arrayLength; i++) {
+    var sid = peripheral.advertisement.serviceUuids[i];
+    var serviceLabel = services.hasOwnProperty(sid) ? sid + "  "+services[sid].name : sid;
+    console.log("service: " + serviceLabel);
+  }
 });
