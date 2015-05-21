d40e98b3224af62033dfe4c8f6813d7f18d6e418
Doug Johnson <finson@whidbey.com>
2015-05-21 07:27:20 -0700

    First complete interaction with NXT brick!

--- nxt/nxt-control.js
+++ nxt/nxt-control.js
@@ -1,86 +1,30 @@
 #!/usr/bin/env node
 
-var Nxt = require('mindstorms_bluetooth');
+var Nxt = require('mindstorms_bluetooth').Nxt;
 
-var nxt = new Nxt("/dev/tty.NXT-DevB");
-nxt.play_tone(440, 1000);
+var nxt = new Nxt("/dev/rfcomm0");
+//var nxt = new Nxt("/org/bluez/569/hci0/dev_00_16_53_1A_59_B9");
 
-var scanStartTime;
-var date = new Date();
-var servers = {};
+console.log(nxt);
 
-noble.on('stateChange', function(state) {
-  console.log("Adapter state change: "+state);
-  if (state === 'poweredOn') {
-    console.log("Power on. Initiate scanning.");
-    noble.startScanning();
-  } else if (state === 'poweredOff') {
-    noble.stopScanning();
-    var scanStopTime = date.getTime();
-    console.log("\nPower off.  Terminate scanning.");
-  }
+nxt.on('getinputvalue', function(data) {
+    if(data[3] == nxt.INPUT_PORT_1){
+        var adc = (data[11] << 8) | data[10];
+        console.log('Touch ADC: ' + adc);
+    }
 });
 
-noble.on('scanStart', function() {
-    console.log("\n======= Scan started.");
-    scanStartTime = date.getTime();
-});
+nxt.sp.on("open", function () {
+    // Init
+    console.log("Robot connected")
+    nxt.play_tone(440, 500);
 
-noble.on('scanStop', function() {
-    var scanStopTime = date.getTime();
-    console.log("\n======= Scan stopped after "+(scanStopTime-scanStartTime)+ " ms.");
-});
+    // Set up hardware
+    nxt.set_input_state(nxt.INPUT_PORT_1, nxt.SWITCH, nxt.BOOLEANMODE);
+    console.log("Hardware set up");
 
-noble.on('discover', function(peripheral) {
-  console.log("\n+++++++ Peripheral discovered.");
-  console.log("   Name: " + peripheral.advertisement.localName);
-  console.log("   UUID: " + peripheral.uuid);
-  console.log("address: " + peripheral.address);
-
-  console.log("------- advertisement object");
-  console.log(peripheral.advertisement);
-
-  console.log("------- peripheral object");
- // console.log(peripheral);
-
-  servers[peripheral.uuid] = peripheral;
-
-  if (peripheral.uuid === 'eb2299410ba7') {
-    console.log("------> connection requested");
-    peripheral.connect();
-  }
-
-});
-
-noble.on('connect', function () {
-  console.log('------- connected');
-//  var pd = servers[]
-  peripheral.discoverServices(); // any service UUID
- // service.discoverIncludedServices(); // any service UUID
-});
-
-noble.on('disconnect', function () {
-  console.log('disconnected');
-});
-
-noble.on('rssiUpdate', function(rssi) {
-  console.log("\nrssi update: "+rssi);
-});
-
-noble.on('servicesDiscover', function(s) {
-  console.log("\nDiscovered services... ");
-
-  // var arrayLength = peripheral.advertisement.serviceUuids.length;
-  // for (var i = 0; i < arrayLength; i++) {
-  //   var sid = peripheral.advertisement.serviceUuids[i];
-  //   var serviceLabel = services.hasOwnProperty(sid) ? sid + "  "+services[sid].name : sid;
-  //   console.log("advertised service: " + serviceLabel);
-  // }
-//  console.log(peripheral);
-
-//    services[sid].discoverCharacteristics() // any characteristic UUID
-});
-
-noble.on('characteristicsDiscover', function(c) {
-  console.log("\ndiscovered characteristic: "+c);
+    // Start interval
+    setInterval(function(){
+        nxt.get_input_values(nxt.INPUT_PORT_1);
+    }, 500);
 });
