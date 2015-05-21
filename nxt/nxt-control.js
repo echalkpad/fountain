8556aee29dc48e8502534ddbe1fa38fd4c224189
Doug Johnson <finson@whidbey.com>
2015-05-21 11:50:28 -0700

    Add battery level getter.

--- nxt/nxt-control.js
+++ nxt/nxt-control.js
@@ -5,12 +5,23 @@ var Nxt = require('mindstorms_bluetooth').Nxt;
 var nxt = new Nxt("/dev/rfcomm0");
 //var nxt = new Nxt("/org/bluez/569/hci0/dev_00_16_53_1A_59_B9");
 
-console.log(nxt);
+//console.log(nxt);
+console.log("===========");
 
 nxt.on('getinputvalue', function(data) {
+  console.log("event: getinputvalue");
     if(data[3] == nxt.INPUT_PORT_1){
         var adc = (data[11] << 8) | data[10];
         console.log('Touch ADC: ' + adc);
+    }
+});
+
+nxt.on('getbatterylevel', function(data) {
+    console.log("event: getbatterylevel");
+    console.log(data);
+    if (data[1] == nxt.EVENTID.getbatterylevel) {
+        var bat = (data[4] << 8) | data[3];
+        console.log('Battery level: '+bat);
     }
 });
 
@@ -25,6 +36,11 @@ nxt.sp.on("open", function () {
 
     // Start interval
     setInterval(function(){
-        nxt.get_input_values(nxt.INPUT_PORT_1);
+        nxt.get_battery_level();
+ //       nxt.get_input_values(nxt.INPUT_PORT_1);
     }, 500);
+
+//    nxt.get_battery_level();
 });
+
+//
