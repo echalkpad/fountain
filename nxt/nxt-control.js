6f8433d5be6b3b3029d9d5d5e764dcbda6aa646b
Doug Johnson <finson@whidbey.com>
2015-05-25 07:50:24 -0700

    Begin four-channel sensor implementation.

--- nxt/nxt-control.js
+++ nxt/nxt-control.js
@@ -6,18 +6,55 @@ var nxt = new Nxt("/dev/rfcomm0");
 var NxtSys = require('./nxtsys.js');
 var nxtSys = new NxtSys(nxt);
 
-function toHex(d) {
-    return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase();
-}
+var Tuxi = require('tuxi');
+var tj = new Tuxi();
 
 //console.log(nxt);
 console.log("===========");
 
+nxt.sp.on("open", function () {
+    // Init
+    console.log("Robot connected")
+    nxt.play_tone(440, 500);
+
+    // Set up hardware
+    nxt.set_input_state(nxt.INPUT_PORT_0, nxt.NO_SENSOR, nxt.RAWMODE);
+    nxt.set_input_state(nxt.INPUT_PORT_1, nxt.NO_SENSOR, nxt.RAWMODE);
+    nxt.set_input_state(nxt.INPUT_PORT_2, nxt.NO_SENSOR, nxt.RAWMODE);
+    nxt.set_input_state(nxt.INPUT_PORT_3, nxt.LOWSPEED_9V, nxt.RAWMODE);
+    console.log("Hardware set up");
+
+    // Start interval
+    setInterval(function(){
+        nxt.get_battery_level();
+ //       nxt.get_input_values(nxt.INPUT_PORT_1);
+ //
+        nxtSys.get_device_info();
+    }, 1000);
+
+});
+
 nxt.on('getinputvalue', function(data) {
-  console.log("event: getinputvalue");
-    if(data[3] == nxt.INPUT_PORT_1){
-        var adc = (data[11] << 8) | data[10];
-        console.log('Touch ADC: ' + adc);
+    if (data[1] == nxtSys.EVENTID.getinputvalue) {
+      if (data[2] != 0) {
+        console.log("Get Input Value: Error. "+nxt.nxt_error_messages[data[2]]);
+      } else {
+        console.log("event: getinputvalue");
+        switch (data[3]) {
+          case nxt.INPUT_PORT_0:
+            break;
+          case nxt.INPUT_PORT_1:
+            break;
+          case nxt.INPUT_PORT_2:
+            break;
+          case nxt.INPUT_PORT_3:
+            break;
+          default:
+            console.log("Error: Unexpected input port value: "+data[3]);
+        }
+        // if(data[3] == nxt.INPUT_PORT_1){
+        // var adc = (data[11] << 8) | data[10];
+        // console.log('Touch ADC: ' + adc);
     }
 });
 
@@ -37,7 +74,7 @@ nxtSys.on('getdeviceinfo',function(data) {
         console.log("Device Info: OK");
         var btAddress = '';
         for (var idx=18; idx<=23; idx++) {
-          btAddress += toHex(data[idx]);
+          btAddress += tj.toHex(data[idx]);
           if (idx != 23) {
             btAddress += ':';
           }
@@ -59,21 +96,4 @@ nxtSys.on('getdeviceinfo',function(data) {
 
 });
 
-nxt.sp.on("open", function () {
-    // Init
-    console.log("Robot connected")
-    nxt.play_tone(440, 500);
 
-    // Set up hardware
-    nxt.set_input_state(nxt.INPUT_PORT_1, nxt.SWITCH, nxt.BOOLEANMODE);
-    console.log("Hardware set up");
-
-    // Start interval
-    setInterval(function(){
-        nxt.get_battery_level();
- //       nxt.get_input_values(nxt.INPUT_PORT_1);
- //       
-        nxtSys.get_device_info();
-    }, 1000);
-
-});
