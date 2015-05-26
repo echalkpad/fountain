19aa904d1e68dc0e702bfcbf9eb2287642816413
Doug Johnson <finson@whidbey.com>
2015-05-26 14:21:28 -0700

    Begin Ultrasonic Sensor implementation.

--- nxt/nxt-control.js
+++ nxt/nxt-control.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
+var nxtCommPort = "/dev/rfcomm0";
+
 var Nxt = require('mindstorms_bluetooth').Nxt;
-var nxt = new Nxt("/dev/rfcomm0");
+var nxt = new Nxt(nxtCommPort);
 
 var NxtSys = require('./nxtsys.js');
 var nxtSys = new NxtSys(nxt);
@@ -13,16 +15,18 @@ var tj = new Tuxi();
 console.log("===========");
 
 nxt.sp.on("open", function () {
-    // Init
-    console.log("Robot connected")
-    nxt.play_tone(440, 500);
+
+    console.log("Robot connected on "+nxtCommPort);
 
     // Set up hardware
+
     nxt.set_input_state(nxt.INPUT_PORT_0, nxt.NO_SENSOR, nxt.RAWMODE);
     nxt.set_input_state(nxt.INPUT_PORT_1, nxt.NO_SENSOR, nxt.RAWMODE);
     nxt.set_input_state(nxt.INPUT_PORT_2, nxt.NO_SENSOR, nxt.RAWMODE);
     nxt.set_input_state(nxt.INPUT_PORT_3, nxt.LOWSPEED_9V, nxt.RAWMODE);
+
     console.log("Hardware set up");
+    nxt.play_tone(440, 500);
 
     // Start interval
     setInterval(function(){
@@ -35,65 +39,81 @@ nxt.sp.on("open", function () {
 });
 
 nxt.on('getinputvalue', function(data) {
-    if (data[1] == nxtSys.EVENTID.getinputvalue) {
-      if (data[2] != 0) {
-        console.log("Get Input Value: Error. "+nxt.nxt_error_messages[data[2]]);
-      } else {
-        console.log("event: getinputvalue");
-        switch (data[3]) {
-          case nxt.INPUT_PORT_0:
-            break;
-          case nxt.INPUT_PORT_1:
-            break;
-          case nxt.INPUT_PORT_2:
-            break;
-          case nxt.INPUT_PORT_3:
-            break;
-          default:
-            console.log("Error: Unexpected input port value: "+data[3]);
-        }
-        // if(data[3] == nxt.INPUT_PORT_1){
-        // var adc = (data[11] << 8) | data[10];
-        // console.log('Touch ADC: ' + adc);
-    }
+  if (data[1] == nxtSys.EVENTID.getinputvalue) {
+    if (data[2] != 0) {
+      console.log("Error. Get Input Value: "+nxt.nxt_error_messages[data[2]]);
+    } else {
+      console.log("event: getinputvalue");
+      switch (data[3]) {
+        case nxt.INPUT_PORT_0:
+          console.log("input port "+data[3]);
+          break;
+        case nxt.INPUT_PORT_1:
+          console.log("input port "+data[3]);
+          break;
+        case nxt.INPUT_PORT_2:
+          console.log("input port "+data[3]);
+          break;
+        case nxt.INPUT_PORT_3:
+          console.log("input port "+data[3]);
+          break;
+        default:
+          console.log("Error: Get Input Value: Unexpected input port value: "+data[3]);
+      }
+      // if(data[3] == nxt.INPUT_PORT_1){
+      // var adc = (data[11] << 8) | data[10];
+      // console.log('Touch ADC: ' + adc);
+  }
 });
 
-nxt.on('getbatterylevel', function(data) {
-    if (data[1] == nxt.EVENTID.getbatterylevel) {
-        var bat = (data[4] << 8) | data[3];
-        console.log('Battery level: '+bat);
+nxt.on('playtone', function(data) {
+  if (data[1] == nxt.EVENTID.playtone) {
+    if (data[2] != 0) {
+      console.log("Error. Play Tone: "+nxt.nxt_error_messages[data[2]]);
     } else {
-      console.log("event: getbatterylevel");
-      console.log(data);
+      console.log("event: Play Tone complete.");
     }
+  } else {
+    console.log("Error. The invoked callback function 'playtone' does not match Event ID "+data[1]+".");
+  }
+})
+
+nxt.on('getbatterylevel', function(data) {
+  if (data[1] == nxt.EVENTID.getbatterylevel) {
+    if (data[2] != 0) {
+      console.log("Error. Get Battery Level: "+nxt.nxt_error_messages[data[2]]);
+    } else {
+      var bat = (data[4] << 8) | data[3];
+      console.log('event: Battery level: '+bat);
+    }
+  } else {
+    console.log("Error. The invoked callback function 'getbatterylevel' does not match Event ID "+data[1]+".");
+  }
 });
 
 nxtSys.on('getdeviceinfo',function(data) {
-    if (data[1] == nxtSys.EVENTID.getdeviceinfo) {
-      if (data[2] == 0) {
-        console.log("Device Info: OK");
-        var btAddress = '';
-        for (var idx=18; idx<=23; idx++) {
-          btAddress += tj.toHex(data[idx]);
-          if (idx != 23) {
-            btAddress += ':';
-          }
-        }
-        var rssi = (data[28] << 8) | data[25];
-        var flash = (data[32] << 8) | data[29];
-        console.log(" Name: "+data.slice(3,18));
-        console.log(" BT address: "+btAddress);
-        console.log(" RSSI: "+rssi);
-        console.log(" Free User Flash: "+flash);
- //       console.log(data);
-      } else {
-        console.log("Device Info: Error. "+nxt.nxt_error_messages[data[2]]);
-      }
+  if (data[1] == nxtSys.EVENTID.getdeviceinfo) {
+    if (data[2] != 0) {
+      console.log("Error. Get Device Info: "+nxt.nxt_error_messages[data[2]]);
     } else {
-      console.log("event: getdeviceinfo");
-      console.log(data);
+      var btAddress = '';
+      for (var idx=18; idx<=23; idx++) {
+        btAddress += tj.toHex(data[idx]);
+        if (idx != 23) {
+          btAddress += ':';
+        }
+      }
+      var rssi = (data[28] << 8) | data[25];
+      var flash = (data[32] << 8) | data[29];
+      console.log("event: Device Info");
+      console.log(" Name: "+data.slice(3,18));
+      console.log(" BT address: "+btAddress);
+      console.log(" RSSI: "+rssi);
+      console.log(" Free User Flash: "+flash);
     }
-
+  } else {
+  console.log("Error. The invoked callback function 'getdeviceinfo' does not match Event ID "+data[1]+".");
+  }
 });
 
 
