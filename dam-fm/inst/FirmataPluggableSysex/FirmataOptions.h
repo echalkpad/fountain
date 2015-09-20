4fc5210906118de689bc85d59cde91e26581ce09
Doug Johnson <finson@whidbey.com>
2015-09-20 13:04:35 -0700

    Start to structure sysex plugin.

---
+++ dam-fm/inst/FirmataPluggableSysex/FirmataOptions.h
@@ -0,0 +1,12 @@
+/*
+  FirmataOptions.h
+
+  This file defines symbols that enable/disable various features of Standard Firmata.
+*/
+
+#ifndef FirmataOptions_h
+#define FirmataOptions_h
+
+#define OPT_ENABLE_SYSEX_HOOK
+
+#endif  /* FirmataOption_h */
