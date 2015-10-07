3dfeac3014f47b953d76324d3a9672b0a6e0d189
Doug Johnson <finson@whidbey.com>
2015-10-07 16:19:03 -0700

    Start definition of byte-order layer

---
+++ dam-fm/inst/typed.c
@@ -0,0 +1,16 @@
+
+void encodeInt8(int8_t *src,int itemCount, char *dst, size_t dstSize);
+void encodeInt16(int16_t *src,int itemCount, char *dst, size_t dstSize);
+void encodeInt32(int32_t *src,int itemCount, char *dst, size_t dstSize);
+
+void encodeUInt8(uint8_t *src,int itemCount, char *dst, size_t dstSize);
+void encodeUInt16(uint16_t *src,int itemCount, char *dst, size_t dstSize);
+void encodeUInt32(uint32_t *src,int itemCount, char *dst, size_t dstSize);
+
+size_t decodeInt8(char *src, int8_t *dst, size_t maxItemCount);
+size_t decodeInt16(char *src, int16_t *dst, size_t maxItemCount);
+size_t decodeInt32(char *src, int32_t *dst, size_t maxItemCount);
+
+size_t decodeUInt8(char *src, uint8_t *dst, size_t maxItemCount);
+size_t decodeUInt16(char *src, uint16_t *dst, size_t maxItemCount);
+size_t decodeUInt32(char *src, uint32_t *dst, size_t maxItemCount);
