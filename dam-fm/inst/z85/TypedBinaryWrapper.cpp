f4da01807e6f3636c3af430bf647060855690d5c
Doug Johnson <finson@whidbey.com>
2015-10-08 19:55:47 -0700

    Implemented all the Typed Binary Wrapper functions.
    No testing yet.

--- dam-fm/inst/z85/TypedBinaryWrapper.cpp
+++ dam-fm/inst/z85/TypedBinaryWrapper.cpp
@@ -1,11 +1,16 @@
 /**
  * The purpose of the functions defined here is to manage differences
- * in byte order (big-endian, little-endian) between two hosts.
+ * in byte order (big-endian, little-endian) between two hosts.  Note
+ * that the actual code represented by htons, ntohs, etc., is determined
+ * at compile time.
  */
 
+#include <string.h>
 #include "SevenBitCodec.h"
 #include "TypedBinaryWrapper.h"
 #include "swappers.h"
+
+// ---------------------------------------------------------------------------
 
 // single byte values are not affected by addressing issues
 
@@ -15,8 +20,8 @@ void encodeInt8(int8_t *src,int itemCount, char *dst, size_t dstSize) {
 
 void encodeUInt8(uint8_t *src,int itemCount, char *dst, size_t dstSize) {
 
-  if ((itemCount % 4) || (dstSize % 5)) return;
-  if (dstSize < (encodedSize(itemCount)+1)) return;
+  if (itemCount % 4) return;
+  if ((encodedSize(itemCount)+1) > dstSize ) return;
 
   byte *from = (byte *)src;
   char *to = dst;
@@ -27,7 +32,8 @@ void encodeUInt8(uint8_t *src,int itemCount, char *dst, size_t dstSize) {
   }
 }
 
-// 2-byte values may need to have their bytes swapped
+// 2-byte values may need to have their bytes swapped depending on host byte
+// order.
 
 void encodeInt16(int16_t *src,int itemCount, char *dst, size_t dstSize) {
   encodeUInt16((uint16_t *)src,itemCount,dst,dstSize);
@@ -37,8 +43,9 @@ void encodeUInt16(uint16_t *src,int itemCount, char *dst, size_t dstSize){
   uint16_t buf[2];
 
   size_t byteCount = 2 * itemCount;
-  if ((byteCount % 4) || (dstSize % 5)) return;
-  if (dstSize < (encodedSize(byteCount)+1)) return;
+
+  if (byteCount % 4) return;
+  if ((encodedSize(byteCount)+1) > dstSize) return;
 
   uint16_t *from = src;
   char *to = dst;
@@ -51,16 +58,91 @@ void encodeUInt16(uint16_t *src,int itemCount, char *dst, size_t dstSize){
   }
 }
 
-// 4-byte values may need to have their bytes swapped
+// 4-byte values may need to have their bytes swapped depending on host byte
+// order.
 
-void encodeInt32(int32_t *src,int itemCount, char *dst, size_t dstSize){}
-void encodeUInt32(uint32_t *src,int itemCount, char *dst, size_t dstSize){}
+void encodeInt32(int32_t *src,int itemCount, char *dst, size_t dstSize){
+    encodeUInt32((uint32_t *)src,itemCount,dst,dstSize);
+}
 
-size_t decodeInt8(char *src, int8_t *dst, size_t maxItemCount){}
-size_t decodeInt16(char *src, int16_t *dst, size_t maxItemCount){}
-size_t decodeInt32(char *src, int32_t *dst, size_t maxItemCount){}
+void encodeUInt32(uint32_t *src,int itemCount, char *dst, size_t dstSize){
+  uint32_t buf;
 
-size_t decodeUInt8(char *src, uint8_t *dst, size_t maxItemCount){}
-size_t decodeUInt16(char *src, uint16_t *dst, size_t maxItemCount){}
-size_t decodeUInt32(char *src, uint32_t *dst, size_t maxItemCount){}
+  size_t byteCount = 4 * itemCount;
 
+  if (byteCount % 4) return;
+  if ((encodedSize(byteCount)+1) > dstSize) return;
+
+  uint32_t *from = src;
+  char *to = dst;
+  for (size_t idx=0; idx<itemCount; idx+=1) {
+      buf = htonl(*from);
+      encodeBytes((byte *)buf, 4, to, 6);
+      from += 1;
+      to += 5;
+  }
+}
+
+// ---------------------------------------------------------------------------
+
+size_t decodeInt8(char *src, int8_t *dst, size_t maxItemCount){
+  return decodeUInt8(src,(uint8_t *)dst,maxItemCount);
+}
+
+size_t decodeUInt8(char *src, uint8_t *dst, size_t maxItemCount){
+
+  if (strlen(src) % 5) return 0;
+  if ((decodedSize(strlen(src))/(sizeof (uint8_t))) > maxItemCount) return 0;
+
+  char *from = src;
+  byte *to = (byte *)dst;
+  for (size_t idx=0; idx<strlen(src); idx+=5) {
+    decodeChars(from,5,to,4);
+    from += 5;
+    to += 4;
+  }
+  return to - dst;
+}
+
+size_t decodeInt16(char *src, int16_t *dst, size_t maxItemCount) {
+    return decodeUInt16(src,(uint16_t *)dst,maxItemCount);
+}
+
+size_t decodeUInt16(char *src, uint16_t *dst, size_t maxItemCount) {
+  uint16_t buf[2];
+
+  if (strlen(src) % 5) return 0;
+  if ((decodedSize(strlen(src))/(sizeof (uint16_t))) > maxItemCount) return 0;
+
+  char *from = src;
+  uint16_t *to = dst;
+  for (size_t idx=0; idx<strlen(src); idx+=5) {
+    decodeChars(from,5,(byte *)buf,4);
+    to[0] = ntohs(buf[0]);
+    to[1] = ntohs(buf[1]);
+    from += 5;
+    to += 2;
+  }
+  return to - dst;  // return item count, NOT byte count
+}
+
+size_t decodeInt32(char *src, int32_t *dst, size_t maxItemCount){
+      return decodeUInt32(src,(uint32_t *)dst,maxItemCount);
+}
+
+size_t decodeUInt32(char *src, uint32_t *dst, size_t maxItemCount){
+  uint32_t buf;
+
+  if (strlen(src) % 5) return 0;
+  if ((decodedSize(strlen(src))/(sizeof (uint32_t))) > maxItemCount) return 0;
+
+  char *from = src;
+  uint32_t *to = dst;
+  for (size_t idx=0; idx<strlen(src); idx+=5) {
+    decodeChars(from,5,(byte *)buf,4);
+    to[0] = ntohl(buf);
+    from += 5;
+    to += 1;
+  }
+  return to - dst;  // return item count, NOT byte count
+}
--- dam-fm/inst/z85/swappers.h
+++ dam-fm/inst/z85/swappers.h
@@ -6,17 +6,17 @@
 #if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 /* The host byte order is the same as network byte order,
    so these functions are all just identity.  */
-# define ntohl(x)        (x)
-# define ntohs(x)        (x)
-# define htonl(x)        (x)
-# define htons(x)        (x)
-# elif __BYTE_ORDER == __LITTLE_ENDIAN
+#   define ntohl(x)        (x)
+#   define ntohs(x)        (x)
+#   define htonl(x)        (x)
+#   define htons(x)        (x)
+# elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #   define ntohl(x)        byteSwap32 (x)
 #   define ntohs(x)        byteSwap16 (x)
 #   define htonl(x)        byteSwap32 (x)
 #   define htons(x)        byteSwap16 (x)
 # else
-# error "Unrecognized __BYTE_ORDER value."
+# error "Unrecognized __BYTE_ORDER__ value."
 # endif
 
 uint16_t byteSwap16(uint16_t a);
